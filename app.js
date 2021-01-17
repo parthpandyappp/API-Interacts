041e68c2ce0398c2f6f1a4cf8f2054261d602636
parthpandyappp <parthpandyappp@gmail.com>
2021-01-17 21:12:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+import {info} from './env.js'
+
+const body = document.querySelector('body')
+const button = document.querySelector('.button')
+const word = document.createElement('h1')
+const def = document.createElement('p')
+
+const randomword = () => {
+    fetch('http://random-word-api.herokuapp.com/word?number=1')
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            word.textContent = response;
+            body.appendChild(word)
+            randomDefinition(word)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+const randomDefinition = (word) => {
+    // console.log(word.textContent)
+    fetch('https://dictionaryapi.com/api/v3/references/collegiate/json/'+(word.textContent)+"?key="+(API_KEY))
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            if(response[0].shortdef!=null){
+                def.textContent = "Definition : " + response[0].shortdef[0];
+            }
+            else{
+                def.textContent = "No definition available"
+            }
+            body.appendChild(def)
+            console.log(response[0].shortdef[0])
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+button.addEventListener('click', function () {
+    randomword();
+})
